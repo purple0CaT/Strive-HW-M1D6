9ef13d0366304fa6f211c72420b9f962a19378f4
Istvan <steven.ford.j@gmail.com>
2021-06-28 09:00:37 +0100

    Solved Ex.3

--- d6.js
+++ d6.js
@@ -104,7 +104,12 @@ console.log(whoIsBigger(2,2))
     Write a function called "splitMe" which receives a string as a parameter and returns an array with every word in that string.
     Ex. splitMe("I love coding") => returns ["I", "Love", "Coding"]
 */
-
+console.log('\n<=== Ex.3  ===>\n')
+function splitMe(a){
+  let x = a.split(' ')
+  return x
+}
+console.log(splitMe('Hello mate, how its goin?'))
 /* Ex.4
     Write a function called "deleteOne" which receives a string and a boolean as parameters. If the boolean value is true it should return the string without the first letter, otherwise it should remove the last one from it.
 */
